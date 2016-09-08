81a82c90a09fa6bd37832664a137eecfc71f73ed
Gareth Maguire <gareth.maguire@sap.com>
2016-09-08 15:17:20 +0100

    Add cocoa pods and and initial code

--- DysonBot/DysonBot/ViewController.swift
+++ DysonBot/DysonBot/ViewController.swift
@@ -1,25 +1,29 @@
-//
-//  ViewController.swift
-//  DysonBot
-//
-//  Created by Gareth Maguire on 08/09/2016.
-//  Copyright © 2016 GJAM. All rights reserved.
-//
-
 import UIKit
+import Moscapsule
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var payloadToggle = true
+
+    @IBAction func buttonTap(_ sender: AnyObject) {
+
+        let mqttConfig = MQTTConfig(clientId: "cid", host: "192.168.1.103", port: 1883, keepAlive: 60)
+
+
+        let payload = payloadToggle ? "{\"Left\":-3000, \"Right\":1000}" : "{\"Left\":0, \"Right\":0}"
+
+        payloadToggle = !payloadToggle
+
+
+        mqttConfig.onPublishCallback = { messageId in
+            NSLog("published (mid=\(messageId))")
+        }
+
+        let mqttClient = MQTT.newConnection(mqttConfig)
+
+        mqttClient.publishString(payload, topic: "command/wheel_speed", qos: 0, retain: false)
+
+        mqttClient.disconnect()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
---
+++ DysonBot/Pods/Target Support Files/Pods-DysonBotUITests/Pods-DysonBotUITests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_DysonBotUITestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DysonBotUITestsVersionString[];
+
