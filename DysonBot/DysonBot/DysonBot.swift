cd57bd6f7470c430b1dce15115d21816ef21b96b
Gareth Maguire <gareth.maguire@sap.com>
2016-09-08 19:59:34 +0100

    Add sensor detection

--- DysonBot/DysonBot/DysonBot.swift
+++ DysonBot/DysonBot/DysonBot.swift
@@ -2,7 +2,6 @@ import Foundation
 import Moscapsule
 
 struct DysonBot: Bot {
-
     let mqttConfig: MQTTConfig
     let mqttClient: MQTTClient
 
@@ -10,14 +9,42 @@ struct DysonBot: Bot {
         mqttConfig = MQTTConfig(clientId: "cid", host: ipAddress, port: 1883, keepAlive: 60)
 
         mqttConfig.onPublishCallback = { messageId in
-            NSLog("published (mid=\(messageId))")
-        }
-
-        mqttConfig.onMessageCallback = { message in
-            NSLog("message (mid=\(message.payloadString))")
+            print("published (mid=\(messageId))")
         }
 
         mqttClient = MQTT.newConnection(mqttConfig)
+
+        let x = self
+        mqttConfig.onMessageCallback = {
+            message in
+            print("message (mid=\(message.payloadString))")
+
+            switch message.topic {
+
+            case "status/psd":
+                let data = DysonBot.parse(jsonPayload: message.payloadString!)!
+
+                for key in data.keys {
+                    if key == "FrontLeft" {
+                        x.proximityDetected?(.frontLeft, data[key]!)
+                    }
+
+                    if key == "FrontRight" {
+                        x.proximityDetected?(.frontRight, data[key]!)
+                    }
+
+                    if key == "BackLeft" {
+                        x.proximityDetected?(.backLeft, data[key]!)
+                    }
+
+                    if key == "BackRight" {
+                        x.proximityDetected?(.backRight, data[key]!)
+                    }
+                }
+            default:
+                break
+            }
+        }
 
         mqttClient.subscribe("status/psd", qos: 0)
     }
@@ -31,4 +58,18 @@ struct DysonBot: Bot {
     var proximityDetected: ((Position, Int) -> Void)?
 
     var bumpDetected: (() -> Void)?
+
+
+    static func parse(jsonPayload: String) -> [String: Int]? {
+        var payload: [String: Int]?
+
+        do {
+            let data = jsonPayload.data(using: .utf8)!
+            payload = try JSONSerialization.jsonObject(with: data, options:JSONSerialization.ReadingOptions()) as? [String: Int]
+        } catch {
+            print(error)
+        }
+
+        return payload
+    }
 }
