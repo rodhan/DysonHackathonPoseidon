8ae554acf9836bae4e1b7907a078fd4784793c7a
Gareth Maguire <gareth.maguire@sap.com>
2016-09-08 15:26:38 +0100

    Get the bot working

--- DysonBot/DysonBot/ViewController.swift
+++ DysonBot/DysonBot/ViewController.swift
@@ -4,25 +4,23 @@ import Moscapsule
 class ViewController: UIViewController {
 
     var payloadToggle = true
+    let mqttConfig = MQTTConfig(clientId: "cid", host: "192.168.1.103", port: 1883, keepAlive: 60)
+    var mqttClient: MQTTClient!
 
-    @IBAction func buttonTap(_ sender: AnyObject) {
-
-        let mqttConfig = MQTTConfig(clientId: "cid", host: "192.168.1.103", port: 1883, keepAlive: 60)
-
-
-        let payload = payloadToggle ? "{\"Left\":-3000, \"Right\":1000}" : "{\"Left\":0, \"Right\":0}"
-
-        payloadToggle = !payloadToggle
-
+    override func viewDidLoad() {
         mqttConfig.onPublishCallback = { messageId in
             NSLog("published (mid=\(messageId))")
         }
 
-        let mqttClient = MQTT.newConnection(mqttConfig)
+        mqttClient = MQTT.newConnection(mqttConfig)
+    }
+
+    @IBAction func buttonTap(_ sender: AnyObject) {
+
+        let payload = payloadToggle ? "{\"Left\":-3000, \"Right\":1000}" : "{\"Left\":0, \"Right\":0}"
+        payloadToggle = !payloadToggle
 
         mqttClient.publishString(payload, topic: "command/wheel_speed", qos: 0, retain: false)
-
-        mqttClient.disconnect()
     }
 }
 
