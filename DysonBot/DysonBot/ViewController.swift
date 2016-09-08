df7c1b92abc345ac85cc213bb4392c76e0284807
Paul Beattie <Paul.Beattie@Manything.com>
2016-09-08 14:19:00 +0100

    Added mutt to view controller

--- DysonBot/DysonBot/ViewController.swift
+++ DysonBot/DysonBot/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Moscapsule
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    // set MQTT Client Configuration
+    let mqttConfig = MQTTConfig(clientId: "cid", host: "test.mosquitto.org", port: 1883, keepAlive: 60)
+    mqttConfig.onPublishCallback = { messageId in
+        NSLog("published (mid=\(messageId))")
+    }
+    mqttConfig.onMessageCallback = { mqttMessage in
+        NSLog("MQTT Message received: payload=\(mqttMessage.payloadString)")
+    }
+    
+    // create new MQTT Connection
+    let mqttClient = MQTT.newConnection(mqttConfig)
+    
+    // publish and subscribe
+    mqttClient.publishString("message", topic: "publish/topic", qos: 2, retain: false)
+    mqttClient.subscribe("subscribe/topic", qos: 2)
+    
+    // disconnect
+    mqttClient.disconnect()
 }
 
