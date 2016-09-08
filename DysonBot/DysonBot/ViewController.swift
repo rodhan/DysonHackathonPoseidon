d58f7657eef26e72305793664989a341a845c89b
Gareth Maguire <gareth.maguire@sap.com>
2016-09-08 14:09:44 +0100

    Add ignore file

---
+++ DysonBot/DysonBot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DysonBot
+//
+//  Created by Gareth Maguire on 08/09/2016.
+//  Copyright © 2016 GJAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
