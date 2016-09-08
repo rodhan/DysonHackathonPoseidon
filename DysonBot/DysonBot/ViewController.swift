d648703e0eadd0610bdb7785c080e5e53992ba56
Gareth Maguire <gareth.maguire@sap.com>
2016-09-08 16:11:19 +0100

    Add sliders

--- DysonBot/DysonBot/ViewController.swift
+++ DysonBot/DysonBot/ViewController.swift
@@ -2,10 +2,15 @@ import UIKit
 import Moscapsule
 
 class ViewController: UIViewController {
+    var leftSpeed = 0;
+    var rightSpeed = 0;
 
-    var payloadToggle = true
     let mqttConfig = MQTTConfig(clientId: "cid", host: "192.168.1.103", port: 1883, keepAlive: 60)
     var mqttClient: MQTTClient!
+
+    @IBOutlet weak var rightTrack: UISlider!
+    @IBOutlet weak var leftTrack: UISlider!
+    @IBOutlet weak var imageView360: UIImageView!
 
     override func viewDidLoad() {
         mqttConfig.onPublishCallback = { messageId in
@@ -14,11 +19,17 @@ class ViewController: UIViewController {
 
         mqttClient = MQTT.newConnection(mqttConfig)
     }
+    
+    @IBAction func rightSlider(_ sender: UISlider){
+        rightSpeed = Int(sender.value)
+        let payload = "{\"Left\":\(leftSpeed), \"Right\":\(rightSpeed)}"
 
-    @IBAction func buttonTap(_ sender: AnyObject) {
+        mqttClient.publishString(payload, topic: "command/wheel_speed", qos: 0, retain: false)
+    }
 
-        let payload = payloadToggle ? "{\"Left\":-3000, \"Right\":1000}" : "{\"Left\":0, \"Right\":0}"
-        payloadToggle = !payloadToggle
+    @IBAction func leftSlider(_ sender: UISlider) {
+        leftSpeed = Int(sender.value)
+        let payload = "{\"Left\":\(leftSpeed), \"Right\":\(rightSpeed)}"
 
         mqttClient.publishString(payload, topic: "command/wheel_speed", qos: 0, retain: false)
     }
